6730cf2b6dd6cdae6cefa84d870684c373f25601
melissaschang <40808810+melissaschang@users.noreply.github.com>
2022-01-10 14:24:25 -0800

    temp1

---
+++ colortrack1.py
@@ -0,0 +1,72 @@
+#credit: https://gist.github.com/pknowledge/aa1469b7ba8cd652adb652d4359ef4f0
+#https://pysource.com/2021/01/28/object-tracking-with-opencv-and-python/
+
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+cap = cv2.VideoCapture(0);
+
+cv2.namedWindow("Tracking")
+cv2.createTrackbar("LH", "Tracking", 93, 255, nothing)
+cv2.createTrackbar("LS", "Tracking", 98, 255, nothing)
+cv2.createTrackbar("LV", "Tracking", 42, 255, nothing)
+cv2.createTrackbar("UH", "Tracking", 110, 255, nothing)
+cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
+
+while True:
+    #frame = cv2.imread('smarties.png')
+    _, frame = cap.read()
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    l_h = cv2.getTrackbarPos("LH", "Tracking")
+    l_s = cv2.getTrackbarPos("LS", "Tracking")
+    l_v = cv2.getTrackbarPos("LV", "Tracking")
+
+    u_h = cv2.getTrackbarPos("UH", "Tracking")
+    u_s = cv2.getTrackbarPos("US", "Tracking")
+    u_v = cv2.getTrackbarPos("UV", "Tracking")
+
+    l_b = np.array([l_h, l_s, l_v])
+    u_b = np.array([u_h, u_s, u_v])
+
+    mask = cv2.inRange(hsv, l_b, u_b)
+
+
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    max_w=0
+    max_h=0
+    max_x=0
+    max_y=0
+
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area>200:   
+            x, y, w, h = cv2.boundingRect(cnt)
+            if (w*h > max_h*max_w):
+                max_h=h
+                max_w=w
+                max_x=x
+                max_y=y
+        
+    cv2.rectangle(frame, (max_x, max_y), (max_x + max_w, max_y + max_h), (0, 255, 0), 3)
+            
+
+    cv2.imshow("frame", frame)
+    cv2.imshow("mask", mask)
+    cv2.imshow("res", res)
+
+   
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
